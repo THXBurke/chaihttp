b7fd5bd70012c8abe9613c1f8de509d7d416f691
THXBurke <rachelwheels@gmail.com>
2016-04-14 11:32:37 -0700

    test trials

--- test/server_test.js
+++ test/server_test.js
@@ -1,3 +1,4 @@
+
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
@@ -5,36 +6,27 @@ const expect = chai.expect;
 const request = chai.request;
 require(__dirname + '/../server');
 
-describe('Craig\'s super awesome HTTP server', () => {
-  it('should accept GET requests to /someroute', (done) => {
-    request('localhost:3000')
-    .get('/someroute')
-    .end((err, res) => {
-      expect(err).to.eql(null);
-      expect(res).to.have.status(200);
-      expect(res.text).to.eql('<h1>wow such GET</h1>');
+describe('vanilla HTTP server should give time or greet person by name', () => {
+  it('should greet someone by name when GET/greet', (done) => {
+    chai.request('localhost:3000')
+      .get('/someroute')
+      .end((err, res) => {
+        if(err) {return err};
+          expect(res).to.have.status(200);
+          expect(res.text).to.eql('howdy, friend');
       done();
     });
   )};
-  it('should four oh four on bad requests', (done) => {
-    request('localhost:3000')
-    .get('/badroute')
+  it('should give back time when GET/time', (done) => {
+    chai.request('localhost:3000')
+    .get('/time')
     .end((err, res) => {
-      expect(res.status).to.eql(404);
-      expect(res.text).eql('this be a 404 msg, yo');
-      done();
+      if(err) {return err};
+        expect(res.status).to.eql(404);
+        expect(res.text).eql('this be a 404 msg, yo');
+        done();
     })
   });
 });
 
-  it('should accept POST requests to /someroute', (done) => {
-    request('localhost:3000')
-    .post('/someroute')
-    .send({'never': 'gonna give you up'})
-    .end((err, res) => {
-      expect(err).to.eql(null);
-      expect(res).to.have.status(200);
-      expect(res.text).to.eql('gonna give you up');
-      done();
-    });
-  });
+  
